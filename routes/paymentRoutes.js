5deffab19eb6fb7c61a062168c646791a73459ba
Hasrh1032 <harshchamola@gmail.com>
2025-10-10 14:13:44 +0400

    added the webhook for stripe

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const { authUser } = require("../middleware/authUser");
-const { createPayment } = require("../controllers/paymentController");
+const { createPayment, callStripeWebhook } = require("../controllers/paymentController");
 
 // POST /pay
 router.post("/pay", authUser, createPayment);
+router.post("/webhook", callStripeWebhook );a
 
 module.exports = router;
