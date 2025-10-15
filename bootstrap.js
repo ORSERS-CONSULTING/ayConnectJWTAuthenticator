be0f3245bbc8f043f64bfbe55fc40fba8e3cd5c8
Hasrh1032 <harshchamola@gmail.com>
2025-10-15 15:45:08 +0400

    add the logic for jwt secret key rotation

--- bootstrap.js
+++ bootstrap.js
@@ -1,7 +1,7 @@
 // bootstrap.js
 require('dotenv').config();
 const loadConfig = require('./config/env');
-const jwtKeyring = require('./lib/jwtKeyring');
+const jwtKeyring = require('./utils/jwtKeyring');
 
 (async () => {
   const cfg = await loadConfig();
