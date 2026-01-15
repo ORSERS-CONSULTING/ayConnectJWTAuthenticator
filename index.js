b38bf77d3eced2d32889ea0d11a51626fa70e52f
Hasrh1032 <harshchamola@gmail.com>
2026-01-15 12:33:39 +0400

    updated index.js

--- index.js
+++ index.js
@@ -174,6 +174,6 @@ io.on("connection", (socket) => {
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, "::", () =>
+server.listen(PORT, () =>
   console.log(`✅ JWT service running on http://${HOST}:${PORT}`)
 );
