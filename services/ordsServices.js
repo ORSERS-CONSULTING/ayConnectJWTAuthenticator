2c9159d112bd9938c47e852a98c46174c59cd838
Hasrh1032 <harshchamola@gmail.com>
2026-02-02 12:41:09 +0400

    updat sms logic

--- services/ordsServices.js
+++ services/ordsServices.js
@@ -50,7 +50,6 @@ async function callGatewayBinary(
 async function callGateway(method, path, { params, data } = {}) {
   const url = `${process.env.GATEWAY_BASE_URL}/${path}`;
   const token = await getIdcsToken(url);
-  console.log("🔑 IDCS TOKEN:", token.slice(0, 30), "...");
   const res = await axios({
     url,
     method,
